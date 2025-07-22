a3ff3ddadd6200aabaa6b8ce635cff79d34dfcb6
PedroVMLopes <pedrovitormlopes@gmail.com>
2025-07-21 22:10:33 -0300

    feat: Iniciative header improvements

--- components/iniciative/IniciativeHeader.tsx
+++ components/iniciative/IniciativeHeader.tsx
@@ -5,16 +5,23 @@ import * as React from "react"
 import {
   DropdownMenu,
   DropdownMenuContent,
+  DropdownMenuGroup,
   DropdownMenuItem,
   DropdownMenuLabel,
+  DropdownMenuPortal,
   DropdownMenuRadioGroup,
   DropdownMenuRadioItem,
   DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
 import { LuChevronsUpDown, LuSwords } from "react-icons/lu";
 import { LucidePlus, LucidePencil } from "lucide-react";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
 
 export default function IniciativeHeader() {
     const [position, setPosition] = React.useState("Encouter 1");
@@ -41,24 +48,65 @@ export default function IniciativeHeader() {
                 align="start"
             >
                 <DropdownMenuLabel className="text-xs opacity-60 font-medium">Active Encounters</DropdownMenuLabel>
+                {/* Encounter Selector */}
                 <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
                     <DropdownMenuRadioItem value={"Encouter 1"}>Encouter 1</DropdownMenuRadioItem>
                     <DropdownMenuRadioItem value={"Encouter 2"}>Encouter 2</DropdownMenuRadioItem>
                     <DropdownMenuRadioItem value={"Encouter 3"}>Encouter 3</DropdownMenuRadioItem>
                 </DropdownMenuRadioGroup>
+
                 <DropdownMenuSeparator />
-                <DropdownMenuItem>
+
+                {/* Create New Players & Enemies */}
+                <DropdownMenuGroup>
+                    <DropdownMenuSub>
+                        <DropdownMenuSubTrigger>Create Player</DropdownMenuSubTrigger>
+                        <DropdownMenuPortal>
+                            <DropdownMenuSubContent>
+                                <div className="flex flex-col gap-1">
+                                    <Input placeholder="Name"></Input>
+                                    <Input placeholder="AC"></Input>
+                                    <Input placeholder="HP"></Input>
+                                    <Input placeholder="Mod"></Input>
+                                    <Button>Add</Button>
+                                </div>
+                            </DropdownMenuSubContent>
+                        </DropdownMenuPortal>
+                    </DropdownMenuSub>
+                    <DropdownMenuSub>
+                        <DropdownMenuSubTrigger>Create Enemy</DropdownMenuSubTrigger>
+                        <DropdownMenuPortal>
+                            <DropdownMenuSubContent>
+                                <div className="flex flex-col gap-1">
+                                    <Input placeholder="Name"></Input>
+                                    <Input placeholder="AC"></Input>
+                                    <Input placeholder="HP"></Input>
+                                    <Input placeholder="Mod"></Input>
+                                    <Button>Add</Button>
+                                </div>
+                            </DropdownMenuSubContent>
+                        </DropdownMenuPortal>
+                    </DropdownMenuSub>
+                </DropdownMenuGroup>
+
+                <DropdownMenuSeparator />
+
+                {/* Add & Edit Encounters */}
+                <DropdownMenuGroup>
+                    <DropdownMenuItem>
                     <div className="flex flex-row gap-2 items-center">
                         <LucidePlus className="ml-auto" />
                         <p className="leading-none">New Encounter</p>
                     </div>
-                </DropdownMenuItem>
-                <DropdownMenuItem className="mt-1">
-                    <div className="flex flex-row gap-2 items-center">
-                        <LucidePencil className="ml-auto size-4" />
-                        <p className="leading-none">Edit Encounter</p>
-                    </div>
-                </DropdownMenuItem>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem className="mt-1">
+                        <div className="flex flex-row gap-2 items-center">
+                            <LucidePencil className="ml-auto size-4" />
+                            <p className="leading-none">Edit Encounter</p>
+                        </div>
+                    </DropdownMenuItem>
+                </DropdownMenuGroup>
+                
             </DropdownMenuContent>
         </DropdownMenu>
     )
