1d4e0e7a947ebfe0cf598d5ddf5e06fb59d76b53
PedroVMLopes <pedrovitormlopes@gmail.com>
2025-07-22 01:47:01 -0300

    fix: zod config

--- lib/zodSchemas.ts
+++ lib/zodSchemas.ts
@@ -1,17 +1,17 @@
 import { z } from "zod";
 
 export const characterSchema = z.object({
-    name: z.string().min(1, "Name is required"),
-    type: z.enum(["player", "enemy", "npc"]),
-    hp: z.coerce.number().min(0, "Invalid HP"),
-    maxHp: z.coerce.number().min(1, "Invalid Max HP"),
-    ac: z.coerce.number().optional(),
-    iniciativeMod: z.coerce.number().optional(),
+  name: z.string().min(1, "Nome é obrigatório"),
+  type: z.enum(["player", "enemy", "npc"]),
+  hp: z.coerce.number().min(0),
+  maxHp: z.coerce.number().min(1),
+  ac: z.coerce.number().optional(),
+  initiative: z.coerce.number().optional(),
 
-    // Optional Fields
-    class: z.string().optional(),
-    level: z.coerce.number().optional(),
-    creatureType: z.string().optional(),
-    cr: z.coerce.number().optional(),
-    notes: z.string().optional(),
-})
+  // Optional Fields  
+  class: z.string().optional(),
+  level: z.coerce.number().optional(),
+  creatureType: z.string().optional(),
+  cr: z.coerce.number().optional(),
+  notes: z.string().optional(),
+});
