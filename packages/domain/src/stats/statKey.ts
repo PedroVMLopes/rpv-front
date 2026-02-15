b87156b42704eb3eb68cb9a50ad74ef688d25668
Pedro Vitor Miranda Lopes <pedrovitormlopes@gmail.com>
2026-02-15 17:25:11 -0300

    Latest update

--- packages/domain/src/stats/statKey.ts
+++ packages/domain/src/stats/statKey.ts
@@ -1,3 +1,6 @@
+// Maybe all of the statkeys should be stored here
+// so items from different systems could be used, just the stats will not change
+
 export type StatKey =
     | "strength"
     | "dexterity"
