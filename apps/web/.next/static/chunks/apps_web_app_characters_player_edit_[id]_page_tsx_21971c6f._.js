2c557adae5eda9a78bfdf6bbd561b3bd2f55c12b
Pedro Vitor Miranda Lopes <pedrovitormlopes@gmail.com>
2026-06-13 00:25:58 -0300

    Enhance character edit page functionality by adding routing and server references. Updated app build and server manifests to include new page, optimized chunk loading, and adjusted font references for consistency.

---
+++ apps/web/.next/static/chunks/apps_web_app_characters_player_edit_[id]_page_tsx_21971c6f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_753db68c._.js",
+  "static/chunks/apps_web_41cb6335._.js"
+],
+    source: "dynamic"
+});
