e4043e5d5c7281ce4ff4818b61e945725e6ef48f
Pedro Vitor Miranda Lopes <pedrovitormlopes@gmail.com>
2026-08-18 12:19:35 -0300

    Combat tab removal because it was redundant. The info is always shown on the header

--- apps/web/components/characters/PlayerSheet/tabs/CombatTab.tsx
+++ apps/web/components/characters/PlayerSheet/tabs/CombatTab.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import type { StoredCharacter } from "@/lib/character/storedCharacter";
-import { ActionStatusRail } from "../combat/ActionStatusRail";
 import { AttacksActionsPanel } from "../combat/AttacksActionsPanel";
 import { DefenseSavesPanel } from "../combat/DefenseSavesPanel";
 import { ConditionsPanel } from "../combat/ConditionsPanel";
@@ -14,7 +13,6 @@ export function CombatTab({ stored }: CombatTabProps) {
     return (
         <div className="grid grid-cols-1 gap-4 lg:grid-cols-[1.4fr_1fr] lg:gap-6">
             <div className="flex flex-col gap-4">
-                <ActionStatusRail stored={stored} />
                 <AttacksActionsPanel stored={stored} />
             </div>
             <div className="flex flex-col gap-4">
